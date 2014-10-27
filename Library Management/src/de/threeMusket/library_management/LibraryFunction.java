79447e8228092421728f026f6b6cce2b299c1826
mazhar <hossain.mazharul@gmail.com>
2014-10-27 11:05:44 +0600

    Update After first edit
    Code changed

--- Library Management/src/de/threeMusket/library_management/LibraryFunction.java
+++ Library Management/src/de/threeMusket/library_management/LibraryFunction.java
@@ -6,6 +6,7 @@
 package de.threeMusket.library_management;
 
 import de.threeMusket.library_management.mysql.MySQLAccess;
+import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,18 +19,75 @@ public class LibraryFunction {
     MySQLAccess dao;
 
     public LibraryFunction() {
-        MySQLAccess dao = new MySQLAccess();
+        dao = new MySQLAccess();
         dao.connectDataBase();
     }
-    public String[][] Browse(){
+
+    public String[][] Browse() {
+        String[][] resultData = null;
         try {
-            String query = "";
-            dao.executeQuery(query);
+            String query = "Select * from books";
+            ResultSet resultSet = dao.executeQuery(query);
+            resultData = dao.readResultData(resultSet);
         } catch (Exception ex) {
             Logger.getLogger(LibraryFunction.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
-    } 
+        return resultData;
+    }
+
+    public String[][] Search(String[] keys, String[] values) {
+        String[][] resultData = null;
+        try {
+            String query = "Select * from books where ";
+            Boolean flag = false;
+            for (int i = 0; i < keys.length; i++) {
+                if (flag) {
+                    query = query + " and " + keys[i] + " = " + values[i];
+                } else {
+                    query = query + keys[i] + " = " + values[i];
+                    flag = true;
+                }
+            }
+            ResultSet resultSet = dao.executeQuery(query);
+            resultData = dao.readResultData(resultSet);
+        } catch (Exception ex) {
+            Logger.getLogger(LibraryFunction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultData;
+    }
+
+    public Boolean Add(String[] keys, String[] values) {
+        Boolean result = false;
+        Boolean flag = false;
+
+        String query = "Insert into books  ( ";
+        for (String key : keys) {
+            if (flag) {
+                query = query + ", " + key;
+            } else {
+                query = query + key;
+                flag = true;
+            }
+        }
+        query = query + " ) Values ( ";
+        flag = false;
+        for (String value : values) {
+            if (flag) {
+                query = query + ", " + value;
+            } else {
+                query = query + value;
+                flag = true;
+            }
+        }
+        query = query + " )";
+        int resultSet = dao.executeQueryUpdate(query);
+
+        if (resultSet > 0) {
+            result = true;
+        }
+
+        return result;
+    }
 
     public static void main(String[] args) throws Exception {
 
--- Library Management/src/de/threeMusket/library_management/mysql/MySQLAccess.java
+++ Library Management/src/de/threeMusket/library_management/mysql/MySQLAccess.java
@@ -59,86 +59,44 @@ public class MySQLAccess {
         }
         return resultSetLocal;
     }
-
-    public void readDataBase() throws Exception {
+    
+    public int executeQueryUpdate(String query) {
+        int resultSetLocal = 0;
         try {
-            // this will load the MySQL driver, each DB has its own driver
-            Class.forName("com.mysql.jdbc.Driver");
-            // setup the connection with the DB.
-            connect = DriverManager
-                    .getConnection("jdbc:mysql://" + dbServer + "/" + dbName + "?"
-                            + "user=" + dbUserID + "&password=" + dbPass);
-
-            // statements allow to issue SQL queries to the database
-            statement = connect.createStatement();
-            // resultSet gets the result of the SQL query
-            resultSet = statement
-                    .executeQuery("select * from FEEDBACK.COMMENTS");
-            writeResultSet(resultSet);
-
-            // preparedStatements can use variables and are more efficient
-            preparedStatement = connect
-                    .prepareStatement("insert into  FEEDBACK.COMMENTS values (default, ?, ?, ?, ? , ?, ?)");
-            // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-            // parameters start with 1
-            preparedStatement.setString(1, "Test");
-            preparedStatement.setString(2, "TestEmail");
-            preparedStatement.setString(3, "TestWebpage");
-            preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-            preparedStatement.setString(5, "TestSummary");
-            preparedStatement.setString(6, "TestComment");
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connect
-                    .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-            resultSet = preparedStatement.executeQuery();
-            writeResultSet(resultSet);
-
-            // remove again the insert comment
-            preparedStatement = connect
-                    .prepareStatement("delete from FEEDBACK.COMMENTS where myuser= ? ; ");
-            preparedStatement.setString(1, "Test");
-            preparedStatement.executeUpdate();
-
-            resultSet = statement
-                    .executeQuery("select * from FEEDBACK.COMMENTS");
-            writeMetaData(resultSet);
-
-        } catch (ClassNotFoundException | SQLException e) {
-            throw e;
-        } finally {
-            close();
+            resultSetLocal = statement.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        return resultSetLocal;
     }
 
-    private void writeMetaData(ResultSet resultSet) throws SQLException {
-        // now get some metadata from the database
-        System.out.println("The columns in the table are: ");
-        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-        for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
-            System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+    private String[][] readMetaData(ResultSet resultSet) {
+        String[][] columnName = null;
+        try {
+            for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
+                columnName[0][i] = resultSet.getMetaData().getColumnName(i + 1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return columnName;
     }
 
-    private void writeResultSet(ResultSet resultSet) throws SQLException {
-        // resultSet is initialised before the first data set
-        while (resultSet.next()) {
-            // it is possible to get the columns via name
-            // also possible to get the columns via the column number
-            // which starts at 1
-            // e.g., resultSet.getSTring(2);
-            String user = resultSet.getString("myuser");
-            String website = resultSet.getString("webpage");
-            String summary = resultSet.getString("summary");
-            String date = resultSet.getString("datum");
-            String comment = resultSet.getString("comments");
-            System.out.println("User: " + user);
-            System.out.println("Website: " + website);
-            System.out.println("Summary: " + summary);
-            System.out.println("Date: " + date);
-            System.out.println("Comment: " + comment);
+    public String[][] readResultData(ResultSet resultSet) {
+        String[][] columnName = null;
+        try {
+            columnName = readMetaData(resultSet);
+            int index = 1;
+            while (resultSet.next()) {
+                for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
+                    columnName[index][i] = resultSet.getString(resultSet.getMetaData().getColumnName(i + 1));
+                }
+                index++;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return columnName;
     }
 
     // you need to close all three to make sure
