13c48f38a2b38717b34dd923f25fcafe527212c2
mazhar69 <hossain.mazharul@gmail.com>
2014-10-29 23:15:15 +0600

    Database
    Database name : manbook
    db username : root
    db pass : nopass123

---
+++ CustomMainGUI/db - manbook/manbook.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v11.33 (32 bit)
+MySQL - 5.6.20 : Database - manbook
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`manbook` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `manbook`;
+
+/*Table structure for table `books` */
+
+DROP TABLE IF EXISTS `books`;
+
+CREATE TABLE `books` (
+  `id` varchar(11) DEFAULT NULL,
+  `BookName` varchar(100) DEFAULT NULL,
+  `Author` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `books` */
+
+insert  into `books`(`id`,`BookName`,`Author`) values ('1','Machine Learning','Tom M Mitchel'),('2','Algorithm','Jor'),('3','Discrete Mathe','Tester');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
