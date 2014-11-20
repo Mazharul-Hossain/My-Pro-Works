8f758b63af6c9ce3b59a2401472ebbc16edb76d0
Farshad Bin Fazle <farshad1563@gmail.com>
2014-11-20 23:09:00 +0600

    Latest GUI
    Let me know if anything else is required.

---
+++ JavaSocket_latest/JavaSocketApplication/src/javasocketapplication/JavaSocketApplication.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Farshad
+ */
+package javasocketapplication;
+
+
+public class JavaSocketApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    //public static void main(String[] args) {
+        // TODO code application logic here
+    //}
+    
+}
